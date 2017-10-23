d98a33e8e40e2a104d1db1fc89da5b23d165ce13
Gerdje-hc <gerdje.hc@gmail.com>
2017-10-23 20:51:00 +0200

    Initial Commit

---
+++ DishLogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DishLogger
+//
+//  Created by Gerd Hung-chu on 23/10/17.
+//  Copyright © 2017 Gerd Hung-chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
